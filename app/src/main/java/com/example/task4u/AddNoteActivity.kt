7417c1f991b96222138fa5af56c46732a397c29c
amith-hasintha <amithhasintha@gmail.com>
2024-05-13 01:29:37 +0530

    Add new lines

---
+++ app/src/main/java/com/example/task4u/AddNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.task4u
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}

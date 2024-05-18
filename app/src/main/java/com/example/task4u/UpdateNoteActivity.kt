702203601f73873756937eddb735f14b01bd5877
amith-hasintha <amithhasintha@gmail.com>
2024-05-18 13:11:00 +0530

    add UpdateNoteActivity.kt file

---
+++ app/src/main/java/com/example/task4u/UpdateNoteActivity.kt
@@ -0,0 +1,40 @@
+package com.example.task4u
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.task4u.databinding.ActivityUpdateNoteBinding
+
+class UpdateNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateNoteBinding
+    private lateinit var db : NoteDatabaseHelper
+    private var noteId: Int = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id", -1)
+        if (noteId == -1){
+            finish()
+            return
+        }
+
+        val note = db.getNoteById(noteId)
+        binding.updateTitleEditText.setText(note.title)
+        binding.updateContentEditText.setText(note.content)
+
+        binding.updateSaveButton.setOnClickListener{
+            val newTitle = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
+            val updatedNote = Note(noteId, newTitle, newContent)
+            db.updateNote(updatedNote)
+            finish()
+            Toast.makeText(this, "Cahnges Saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+}

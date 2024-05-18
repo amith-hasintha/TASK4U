f4f98e40cd83c3a9656944bc44f24289b855ccd5
amith-hasintha <amithhasintha@gmail.com>
2024-05-18 13:00:38 +0530

    add finalized MainActivity.kt and activity_main.xml files

--- app/src/main/java/com/example/task4u/MainActivity.kt
+++ app/src/main/java/com/example/task4u/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.task4u
 //vgvgvgvgjjknbj
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.task4u.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db:NoteDatabaseHelper
+    private lateinit var noteAdapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+        noteAdapter = NoteAdapter(db.getAllNotes(), this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = noteAdapter
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume(){
+        super.onResume()
+        noteAdapter.refreshData(db.getAllNotes())
     }
 }
